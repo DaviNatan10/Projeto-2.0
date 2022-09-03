15b05fdc3c5fa0d7dc8f6499c41a73a79fb60682
uFelippeSilva <112040769+uFelippeSilva@users.noreply.github.com>
2022-09-03 14:57:00 -0300

    Add files via upload

---
+++ PROJETO.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+int main() {
+
+    void tela_principal(void);
+    tela_principal();
+    void tela_sobre(void);
+    tela_sobre();
+    void tela_dupla(void);
+    tela_dupla();
+    
+    return 0;
+}
+
+void tela_principal(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                             ///\n");
+    printf("///         SIG-Pet um sistema de agendamento de consultas para pets            ///\n");
+    printf("///                                                                             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                             ///\n");
+    printf("///            = = = = sistema de agendamento de consulta para pets = = = =     ///\n");
+    printf("///           |                                                             |   ///\n");
+    printf("///           |           1. cadastro cliente                               |   ///\n");
+    printf("///           |           2. cadastro do pet                                |   ///\n");
+    printf("///           |           3. agendamentos horarios                          |   ///\n");
+    printf("///           |           0. Sair                                           |   ///\n");
+    printf("///           |                                                             |   ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =    ///\n");
+    printf("///                                                                             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+void tela_sobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                             ///\n");
+    printf("///             As clinicas para atendimento a pequenos animais                 ///\n");
+    printf("///              sao cada vez mais populares.                                   ///\n");
+    printf("///             O presente projeto tem como objetivo propor um progama          ///\n");
+    printf("///              de computaodr que implemente funcionalidades basicas           ///\n");
+    printf("///              relacionas ao agendamento de consultas de pequenos animais,    ///\n");
+    printf("///              armazenando tanto informacoes do pet quanto do seu dono.       ///\n");
+    printf("///                                                                             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+}
+
+void tela_dupla(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                             ///\n");
+    printf("///            = = = Sistema de agendamento de consultas para pet = = =         ///\n");
+    printf("///           |                                                         |       ///\n");
+    printf("///           |   Este projeto exemplo foi desenvolvido por:            |       ///\n");
+    printf("///           |                                                         |       ///\n");
+    printf("///           |    Davi Natan Vieira de oliveira                        |       ///\n");
+    printf("///           |     Matricula:20220055255                               |       ///\n");
+    printf("///           |    Felippe da Silva Guedes                              |       ///\n");
+    printf("///           |     Matricula:20220068057                               |       ///\n");
+    printf("///           |                                                         |       ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = = = = =        ///\n");
+    printf("///                                                                             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+} 
